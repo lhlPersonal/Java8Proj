c4ff3ba618f26f6b6e0e8e3ca5cb05bed6c1ee4e
lhlPersonal <lhlPersonal@qq.com>
2015-06-12 18:17:09 +0800

    listener.

--- src/com/lhl/servlet/FirstListener.java
+++ src/com/lhl/servlet/FirstListener.java
@@ -7,48 +7,42 @@ import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-public class FirstListener implements HttpSessionListener,ServletContextListener,HttpSessionAttributeListener {
+public class FirstListener implements HttpSessionListener,
+		ServletContextListener, HttpSessionAttributeListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		// TODO Auto-generated method stub
 		System.out.println("context Destroyed");
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		// TODO Auto-generated method stub
 		System.out.println("context Initialized");
 	}
 
 	@Override
 	public void sessionCreated(HttpSessionEvent se) {
-		// TODO Auto-generated method stub
-		System.out.println("session Id:"+se.getSession().getId());
-		System.out.println("session Created:"+se.getSession().getAttribute("session1"));
+		System.out.println("session Id:" + se.getSession().getId());
+		System.out.println("session Created:"
+				+ se.getSession().getAttribute("session1"));
 	}
 
 	@Override
 	public void sessionDestroyed(HttpSessionEvent se) {
-		// TODO Auto-generated method stub
 		System.out.println("session Destroyed");
 	}
 
 	@Override
 	public void attributeAdded(HttpSessionBindingEvent event) {
-		// TODO Auto-generated method stub
-		System.out.println("session Created:"+event.getSession().getAttribute("session1"));
+		System.out.println("session Created:"
+				+ event.getSession().getAttribute("session1"));
 	}
 
 	@Override
 	public void attributeRemoved(HttpSessionBindingEvent event) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void attributeReplaced(HttpSessionBindingEvent event) {
-		// TODO Auto-generated method stub
-		
 	}
 }
