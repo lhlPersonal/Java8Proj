2235089c049bc84f2dff33146e42fc42cdc45007
lhlPersonal <lhlPersonal@qq.com>
2015-08-11 15:41:32 +0800

    edit some files.

--- src/com/lh/java8Base/Interest.java
+++ src/com/lh/java8Base/Interest.java
@@ -1,16 +1,5 @@
 package com.lh.java8Base;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.nio.charset.Charset;
-
-import javax.inject.Qualifier;
-
-import org.springframework.stereotype.Component;
-
-import com.opensymphony.xwork2.inject.Inject;
 
 public class Interest {
 
--- src/com/lhl/ssh/CustomAnnotation.java
+++ src/com/lhl/ssh/CustomAnnotation.java
@@ -5,13 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Target({ ElementType.FIELD, ElementType.TYPE, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
@@ -59,31 +54,4 @@ class C {
 	public void show() {
 		a.OutMsg();
 	}
-}
-
-class D extends HandlerInterceptorAdapter{
-
-	@Override
-	public void afterCompletion(HttpServletRequest request,
-			HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-		// TODO Auto-generated method stub
-		super.afterCompletion(request, response, handler, ex);
-	}
-
-	@Override
-	public void postHandle(HttpServletRequest request,
-			HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		super.postHandle(request, response, handler, modelAndView);
-	}
-
-	@Override
-	public boolean preHandle(HttpServletRequest request,
-			HttpServletResponse response, Object handler) throws Exception {
-		// TODO Auto-generated method stub
-		return super.preHandle(request, response, handler);
-	}
-	
 }
