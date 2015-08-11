6c400adbac30dab5678e6699b67f0f46085587d9
lhlPersonal <lhlPersonal@qq.com>
2015-08-11 15:41:32 +0800

    edit some files.

--- src/com/lhl/ssh/CustomAnnotation.java
+++ src/com/lhl/ssh/CustomAnnotation.java
@@ -60,30 +60,3 @@ class C {
 		a.OutMsg();
 	}
 }
-
-class D extends HandlerInterceptorAdapter{
-
-	@Override
-	public void afterCompletion(HttpServletRequest request,
-			HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-		// TODO Auto-generated method stub
-		super.afterCompletion(request, response, handler, ex);
-	}
-
-	@Override
-	public void postHandle(HttpServletRequest request,
-			HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		super.postHandle(request, response, handler, modelAndView);
-	}
-
-	@Override
-	public boolean preHandle(HttpServletRequest request,
-			HttpServletResponse response, Object handler) throws Exception {
-		// TODO Auto-generated method stub
-		return super.preHandle(request, response, handler);
-	}
-	
-}
